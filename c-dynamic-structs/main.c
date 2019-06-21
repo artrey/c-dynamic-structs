52c8804db7f4ac3b95596c1b5e83a84b11be6b04
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2019-06-21 09:58:58 +0300

    Added some examples

--- c-dynamic-structs/main.c
+++ c-dynamic-structs/main.c
@@ -1,9 +1,15 @@
 #include "vector/example.h"
+#include "list/example.h"
 
 int main(int argc, char** argv)
 {
 	vector_int_example();
 	vector_double_example();
 	vector_string_example();
+
+	list_int_example();
+	list_double_example();
+	list_string_example();
+
 	return 0;
 }
